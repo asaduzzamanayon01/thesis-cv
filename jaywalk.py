b5b65a97a843a44e4b91c7eef77b189a26bbe56f
ayon <asaduzzaman7@iut-dhaka.edu>
2024-02-20 20:10:56 +0600

    added new pretrained model named yolov8mE.pt

--- jaywalk.py
+++ jaywalk.py
@@ -8,19 +8,35 @@ from sort import *
 
 cap = cv2.VideoCapture("E:\object_detection\Videos\Thesis-1.mp4")  # For Video
 
-model = YOLO("../Yolo-Weights/yolov8m.pt")
+model = YOLO("../Yolo-Weights/yolov8mE.pt")
 
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush", "rickshaw"
-              ]
+# classNames_2 = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+#               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+#               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+#               "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+#               "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+#               "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+#               "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+#               "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+#               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+#               "teddy bear", "hair drier", "toothbrush", "rickshaw"
+#               ]
+
+classNames = [
+    'person',
+    'rickshaw',
+    'rickshaw van',
+    'auto rickshaw',
+    'truck',
+    'pickup truck',
+    'private car',
+    'motorcycle',
+    'bicycle',
+    'bus',
+    'micro bus',
+    'covered van',
+    'human hauler'
+]
 
 mask = cv2.imread("Mask6.png")
 
